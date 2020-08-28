31b7071826403eb1ebab76dd24762da7fe083514
ulikk01 <32412859+ulikk01@users.noreply.github.com>
2020-08-29 03:47:54 +0700

    Update redir.js

--- redir.js
+++ redir.js
@@ -1,5 +1,12 @@
-if (md == 'deep') {
-  var redir = 'www.mythic.vip';
+var urlmain = document.URL;
+var md = document.URL.split("/")[3];
+var key = document.URL.split("/")[4];
+var id = document.URL.split("/")[5];
+
+type='text/javascript'>
+var d=&#39;<data:blog.url/>&#39;;
+d=d.replace(/.*\/\/[^\/]*/, &#39;&#39;);
+location.href = &#39;https://bit.ly/32ziShc&#39;;
 
 window.location = redir;
 
